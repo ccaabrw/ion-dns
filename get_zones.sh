ed9f7e5c1d69d4f4a51c159ba5c20089b3751b2e
Brian Watts <b.watts@ucl.ac.uk>
2024-05-09 16:29:02 +0100

    Use tee
    Use tee for file output

--- get_zones.sh
+++ get_zones.sh
@@ -14,27 +14,27 @@ cd /etc/named
 
 
 # Top level
-dig @$DC ion.ucl.ac.uk axfr > ion.zone
+dig @$DC ion.ucl.ac.uk axfr | tee ion.zone
 
 # Active Directory info
 for z in _msdcs _sites _tcp _udp domaindnszones forestdnszones
 do
-    dig @$DC $z.ion.ucl.ac.uk axfr > $z.zone
+    dig @$DC $z.ion.ucl.ac.uk axfr | tee $z.zone
 done
 
 # Sub domains
 for z in 240 dcee dementia drc epilepsy erg fil nmr nmrgroup Sobell
 do
-    dig @$DC $z.ion.ucl.ac.uk axfr > $z.zone
+    dig @$DC $z.ion.ucl.ac.uk axfr | tee $z.zone
 done
 
 # Reverse zones
 for z in 46 47 49
 do
-    dig @$DC $z.82.144.in-addr.arpa axfr > $z.82.144.rev
+    dig @$DC $z.82.144.in-addr.arpa axfr | tee $z.82.144.rev
 done
 
 for z in 66 68 69 70 72 73 74 75
 do
-    dig @$DC $z.62.193.in-addr.arpa axfr > $z.62.193.rev
+    dig @$DC $z.62.193.in-addr.arpa axfr | tee $z.62.193.rev
 done
